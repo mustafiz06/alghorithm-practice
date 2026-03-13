62948734978c1e291f0c041893c18947eaaff67c
mustafiz06 <themustafiz06@gmail.com>
2026-03-13 21:14:25 +0600

    18th

---
+++ Tetranacci_Number_I.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> dp;
+
+int tetranacci(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    if (n == 1 || n == 2)
+    {
+        return 1;
+    }
+    if (n == 3)
+    {
+        return 2;
+    }
+    if (dp[n] != -1)
+    {
+        return dp[n];
+    }
+
+    
+    dp[n] = tetranacci(n - 1) + tetranacci(n - 2) + tetranacci(n - 3) + tetranacci(n - 4);
+    return dp[n];
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    dp.resize(n + 1, -1);
+    cout << tetranacci(n) << endl;
+    return 0;
+}
